aa718767b6769ded1ab87a47ee3aafd233f77e50
Imperador <147494303+TiranoZero@users.noreply.github.com>
2025-12-09 17:21:27 -0300

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/status', (req, res) => {
   res.json({ listeners: clients.size, broadcasting: !!broadcasting });
 });
 
-app.get('/stream', (req, res) => {
+app.get('/', (req, res) => {
   // registro do cliente
   try { req.socket.setKeepAlive(true, 60000); } catch(_) {}
   try { res.setMaxListeners && res.setMaxListeners(0); } catch(_) {}
